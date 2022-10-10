a0e43c49e10de17483c399da6c4e8b55fd625297
mjrdev <jrgomesdev@gmail.com>
2022-10-10 00:15:49 -0300

    page of coin quotes

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Quotes from './components/Quotes.vue'
+
+</script>
+
+<template>
+  <main class="flex p-6">
+    <Quotes/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Quotes.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import axios from 'axios'
+
+let text = 'ok'
+
+    
+async function get() {
+    const get = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    console.log(get);
+}
+
+function seti(): void {
+  text = 'new';
+}
+
+</script>
+
+<template>
+  <div class="item">
+    <h1 class="text-3xl font-bold underline">{{text}}</h1>
+    <h3 @click="seti()">$1,00 > R$5,00</h3>
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
