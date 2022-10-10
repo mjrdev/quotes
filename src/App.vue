d2086a54d877eedfcacdccf5983e87908bd47bda
mjrdev <jrgomesdev@gmail.com>
2022-10-10 11:34:06 -0300

    prepare for tailwind

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Quotes from './components/Quotes.vue'
 </script>
 
 <template>
-  <main class="flex p-6">
+  <main class="container mx-auto flex justify-center align-middle columns-1 object-center h-full">
     <Quotes/>
   </main>
 </template>
--- src/components/Quotes.vue
+++ src/components/Quotes.vue
@@ -1,24 +1,32 @@
 <script setup lang="ts">
 import axios from 'axios'
 
-let text = 'ok'
+interface IData {
+  title: string,
+  coinOne: number,
+  coinTwo: number,
+  simbol: string
+}
 
+const data: IData = {
+  title: 'Contação em tempo real',
+  coinOne: 1.00,
+  coinTwo: 1.00,
+  simbol: "$"
+}
     
 async function get() {
     const get = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
     console.log(get);
 }
 
-function seti(): void {
-  text = 'new';
-}
 
 </script>
 
 <template>
-  <div class="item">
-    <h1 class="text-3xl font-bold underline">{{text}}</h1>
-    <h3 @click="seti()">$1,00 > R$5,00</h3>
+  <div class="item flex flex-col justify-center">
+    <h1 class="text-3xl font-bold">{{data.title}}</h1>
+    <h3>$1,00 > R$5,00</h3>
   </div>
 </template>
 
